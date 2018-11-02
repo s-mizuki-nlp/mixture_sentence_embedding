9beb0c028ff9ccfe1a8d870d767859fe81e05b49
Sakae Mizuki <mizuki.s.aa@m.titech.ac.jp>
2018-11-02 22:54:46 +0900

    feat: implemented self-attentive input-less lstm decoder

--- model/decoder.py
+++ model/decoder.py
@@ -5,8 +5,58 @@ import os, sys, io
 
 import torch
 from torch import nn
+import torch.nn.functional as F
+from typing import Optional
+from .attention import SimpleGlobalAttention
 
-class SelfAttLSTMDecoder(nn.Module):
+class SelfAttentiveLSTMDecoder(nn.Module):
 
+    def __init__(self, n_dim_lstm_hidden: int, n_dim_memory: int,
+                 attention_layer: Optional[SimpleGlobalAttention] = None):
 
+        super(__class__, self).__init__()
 
+        self._n_dim_lstm_hidden = n_dim_lstm_hidden
+        self._n_dim_memory = n_dim_memory
+
+        if attention_layer is not None:
+            self._attention_layer = attention_layer
+        else:
+            n_dim_query = n_dim_lstm_hidden * 2
+            self._attention_layer = SimpleGlobalAttention(dim_query=n_dim_query, dim_key=n_dim_memory)
+
+        self._lstm_cell = nn.LSTMCell(input_size=n_dim_memory, hidden_size=n_dim_lstm_hidden)
+
+    def _init_state(self, size: int):
+        h_0 = torch.zeros(size, self._n_dim_lstm_hidden)
+        c_0 = torch.zeros(size, self._n_dim_lstm_hidden)
+        return h_0, c_0
+
+    def forward(self, z_latent: torch.Tensor, n_step: int) -> torch.Tensor:
+        """
+        completely input-less sequence decoder.
+
+        :param z_latent: latent tensor; set of latent vectors. shape = (N_batch, N_sample, N_dim_latent)
+        :param n_step: maximum sequence length
+
+        :return tensor of output state. shape = (N_batch, N_step, N_dim_lstm_hidden)
+        """
+        n_batch = z_latent.shape[0]
+        h_t, c_t = self._init_state(size=n_batch)
+        lst_h_t = []
+
+        # calculate each step
+        for t in range(n_step):
+            # state vector
+            v_t = torch.cat((h_t,c_t), dim=-1)
+            # calculate context vector using attention layer with state vector as query
+            x_dec_t, x_dec_t_attn = self._attention_layer.forward(source=v_t, memory_bank=z_latent)
+            # calculate next step
+            h_t, c_t = self._lstm_cell(x_dec_t, (h_t, c_t))
+            # store output state vector
+            lst_h_t.append(h_t)
+
+        # stack it
+        t_dec_h = torch.stack(lst_h_t, dim=1)
+
+        return t_dec_h
