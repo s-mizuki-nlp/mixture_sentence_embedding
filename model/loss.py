fix: fixed default loss value; it doesn't affect gradient value.

model/loss.py
@@ -77,7 +77,7 @@ class EmpiricalSlicedWassersteinDistance(_Loss):
         n_mb, n_dim = input.shape
 
         # sliced wesserstein distance
-        loss = torch.tensor(1., dtype=torch.float, requires_grad=True)
+        loss = torch.tensor(0., dtype=torch.float, requires_grad=True)
         for t in range(self._n_slice):
             t_theta = self._sample_circular(n_dim=n_dim)
             x_t,_ = torch.matmul(input, t_theta).topk(k=n_mb)