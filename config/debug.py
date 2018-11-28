1e82a4fb8293d7bf1eab2d8648aad0fd788bacb5
Sakae Mizuki <mizuki.s.aa@m.titech.ac.jp>
2018-11-28 22:58:54 +0900

    refactor: updated configuration parameters

--- config/debug.py
+++ config/debug.py
@@ -6,11 +6,11 @@ import torch.optim
 
 highway = False
 bidirectional = True
-n_dim_latent = 10
-n_dim_lstm_hidden = 10
-n_dim_embedding = 10
+n_dim_latent = 100
+n_dim_lstm_hidden = 100
+n_dim_embedding = 100
 n_dim_lstm_output = n_dim_lstm_hidden * (bidirectional + 1)
-n_gmm_component = 4
+n_gmm_component = 8
 
 cfg_auto_encoder = {
     "encoder": {
@@ -58,7 +58,7 @@ cfg_auto_encoder = {
     },
     "loss": {
         "empirical_wasserstein": {
-            "n_slice":10
+            "n_slice":50
         },
         "kldiv": {
             "enabled":True,
@@ -80,9 +80,9 @@ cfg_corpus = {
 
 cfg_optimizer = {
     "n_epoch":10,
-    "n_minibatch":10,
+    "n_minibatch":100,
     "optimizer":torch.optim.Adam,
     "lr":0.01,
     # "validation_split":0.0,
-    "validation_interval":5
+    "validation_interval":20
 }
