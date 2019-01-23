d9e5150412c7a7cdedabaea1b64454f334deaac3
Sakae Mizuki <mizuki.s.aa@m.titech.ac.jp>
2019-01-23 23:01:49 +0900

    feat: updated tile plot

--- visualization.py
+++ visualization.py
@@ -1,20 +1,38 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 from matplotlib import pyplot as plt
+from matplotlib import ticker
+import numpy as np
 
-def tile_plot(mat_x):
-    fig, ax = plt.subplots()
-    ax.imshow(mat_x, interpolation='nearest')
-    numrows, numcols = mat_x.shape
+_DEFAULT_FIG_SIZE = (6,6)
 
-    def format_coord(x, y):
-        col = int(x + 0.5)
-        row = int(y + 0.5)
-        if col >= 0 and col < numcols and row >= 0 and row < numrows:
-            z = mat_x[row, col]
-            return 'x=%1.4f, y=%1.4f, z=%1.4f' % (x, y, z)
-        else:
-            return 'x=%1.4f, y=%1.4f' % (x, y)
+def tile_plot(mat_dist, fig_and_ax=None, lst_ticker_x=None, lst_ticker_y=None, cmap="Reds", **kwargs):
 
-    ax.format_coord = format_coord
-    plt.show()
+    @ticker.FuncFormatter
+    def major_formatter(x, pos):
+        return f"{x+0.5:2.0f}"
+
+    if fig_and_ax is None:
+        fig, ax = plt.subplots(figsize=_DEFAULT_FIG_SIZE)
+    else:
+        fig, ax = fig_and_ax[0], fig_and_ax[1]
+
+    n_x, n_y = mat_dist.shape
+    mesh_x, mesh_y = np.meshgrid(np.arange(n_x+1), np.arange(n_y+1))
+
+    ax.pcolormesh(mesh_x, mesh_y, mat_dist.T, cmap=cmap, **kwargs)
+    # adjust axes labels
+    ## X=axis
+    ax.xaxis.set_major_locator(ticker.FixedLocator(np.arange(0.5, n_x, 1.0)))
+    if lst_ticker_x is None:
+        ax.xaxis.set_major_formatter(major_formatter)
+    else:
+        ax.xaxis.set_major_formatter(ticker.FixedFormatter(lst_ticker_x))
+    ## Y-axis
+    ax.yaxis.set_major_locator(ticker.FixedLocator(np.arange(0.5, n_y, 1.0)))
+    if lst_ticker_y is None:
+        ax.yaxis.set_major_formatter(major_formatter)
+    else:
+        ax.yaxis.set_major_formatter(ticker.FixedFormatter(lst_ticker_y))
+
+    return fig, ax
