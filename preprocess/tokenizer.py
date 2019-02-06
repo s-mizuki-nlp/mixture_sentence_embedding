284ef0ad8ba18c97d233cb814253eb98af0cc7e6
Sakae Mizuki <mizuki.s.aa@m.titech.ac.jp>
2019-02-06 19:16:49 +0900

    refactor: implemented wordpiece tokenizer wrapper class

--- preprocess/tokenizer.py
+++ preprocess/tokenizer.py
@@ -4,6 +4,7 @@
 import os, sys, io
 from abc import ABCMeta, abstractmethod
 from typing import List
+from .wordpiece import FullTokenizer
 
 class AbstractTokenizer(object):
 
@@ -17,7 +18,7 @@ class AbstractTokenizer(object):
         pass
 
     def tokenize(self, corpus):
-        if iter(corpus) is iter(corpus):
+        if (iter(corpus) is iter(corpus)) or isinstance(corpus, str):
             raise TypeError("`corpus` must be a container.")
         for sentence in corpus:
             yield self.tokenize_single(sentence)
@@ -25,8 +26,20 @@ class AbstractTokenizer(object):
 
 class CharacterTokenizer(AbstractTokenizer):
 
-    def __init__(self, separator: str=" "):
+    def __init__(self, separator: str=" ", do_lower_case = False):
         self._sep = separator
+        self._do_lower_case = do_lower_case
 
     def tokenize_single(self, sentence):
+        if self._do_lower_case:
+            sentence = sentence.lower()
         return sentence.split(self._sep)
+
+
+class WordPieceTokenizer(AbstractTokenizer):
+
+    def __init__(self, vocab_file: str, do_lower_case = False):
+        self._tokenizer = FullTokenizer(vocab_file, do_lower_case)
+
+    def tokenize_single(self, sentence):
+        return self._tokenizer.tokenize(sentence)
